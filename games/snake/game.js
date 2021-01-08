ef758c83144f9faf46bfacbd910deb368ff09618
joseywoermann <jcw05@gmx.de>
2021-01-08 22:21:54 +0100

    started snake-development

---
+++ games/snake/game.js
@@ -0,0 +1,25 @@
+import { update as updateSnake, draw as drawSnake, SNAKE_SPEED } from './snake.js'
+
+let lastRenderTime = 0
+const gameBoard = document.getElementById('game-board')
+function main(currentTime) {
+  window.requestAnimationFrame(main)
+  const sencondsSinceLastRender = (currentTime - lastRenderTime) / 1000
+  if (sencondsSinceLastRender < 1 / SNAKE_SPEED) return
+
+  lastRenderTime = currentTime
+
+  update()
+  draw()
+}
+
+window.requestAnimationFrame(main)
+
+
+function update() {
+  updateSnake()
+}
+
+function draw() {
+  drawSnake(gameBoard)
+}
